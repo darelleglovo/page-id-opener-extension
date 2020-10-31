8f141119ed1635f90fba4c354501484949f44e17
Darelle Glovo <darelleglovo@MacBook-Pro.local>
2020-11-01 06:02:25 +0800

    initial functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { ChangeEvent, useState } from "react";
+import { Button, TextField, Grid } from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      color: "white",
+      height: theme.spacing(43.75),
+      width: theme.spacing(37.5),
+      padding: theme.spacing(2, 1),
+    },
+  });
+});
 
 function App() {
+  const classes = useStyles();
+  const [url, setUrl] = useState<String>("");
+  const [pageId, setPageId] = useState<String>("");
+
+  const handleButtonOnClick = () => {
+    window.open(`${url}${pageId}`);
+    return;
+  };
+
+  const handleURLOnChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setUrl(event.currentTarget.value);
+  };
+
+  const handlePageIdOnChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setPageId(event.currentTarget.value);
+  };
+
   return (
-    <div className="App">
-      test123-test
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="URL"
+            variant="outlined"
+            size="small"
+            onChange={handleURLOnChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="ID"
+            variant="outlined"
+            size="small"
+            onChange={handlePageIdOnChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary" size="small" onClick={handleButtonOnClick}>
+            Open
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
