8d65e6c73a3d52a2005fb15a499e858ad275bd2d
Darelle Glovo <darelleglovo@MacBook-Pro.local>
2020-11-01 05:20:38 +0800

    downgrade react and typescript for material-ui compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       test123-test
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
